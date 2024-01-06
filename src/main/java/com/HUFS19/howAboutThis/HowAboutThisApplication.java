0172dc6fbc5e24acd86fc24952b22eae4fd06f9d
JH CHO <junh9541@gmail.com>
2024-01-06 17:29:34 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/HUFS19/howAboutThis/HowAboutThisApplication.java
@@ -0,0 +1,13 @@
+package com.HUFS19.howAboutThis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowAboutThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowAboutThisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HUFS19/howAboutThis/HowAboutThisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HUFS19.howAboutThis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowAboutThisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
